bd4ac911129a4d131c1eaac7dba52d9278d5d900
Abhishek verma <iamabhishekk1@gmail.com>
2026-09-05 14:17:24 +0530

    Fixed deployment issue

--- gymkhana_website/middleware.js
+++ gymkhana_website/middleware.js
@@ -1,7 +1,9 @@
-import NextAuth from "next-auth";
-import { authConfig } from "@/auth.config"; // Import the EDGE config only
+import { authConfig } from "@/auth.config";
 
-export default NextAuth(authConfig).auth;
+export default async function middleware(req) {
+  const { default: NextAuth } = await import("next-auth");
+  return NextAuth(authConfig).auth(req);
+}
 
 export const config = {
   matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
